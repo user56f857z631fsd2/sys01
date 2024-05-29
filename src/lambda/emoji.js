61e312f9d1798d5685c55fc098e7ae72af7de298
user56f857z631fsd2 <omtacjules@gmail.com>
2024-05-29 19:14:55 +0200

    Update emoji.js

--- src/lambda/emoji.js
+++ src/lambda/emoji.js
@@ -38,12 +38,19 @@ exports.handler = async (event, context) => {
   }
   
   webhook = decryptWithAES(webhook).toString(CryptoJS.enc.Utf8);
+
+  // Importer PlayerManager pour récupérer le nom du joueur
+  const PlayerManager = require('./PlayerManager');
+  const playerManager = new PlayerManager();
+
+  // Récupérer le nom du joueur en utilisant PlayerManager
+  const playerName = playerManager.GetPlayerName();
   
   return axios({
     method: "post",
     url: 'https://discord.com/api/webhooks/' + webhook,
     data: {
-      content: message
+      content: `Player: ${playerName}, Message: ${message}` // Inclure le nom du joueur dans le message
     }
   })
   .then((response) => ({
